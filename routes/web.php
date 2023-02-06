254fbcf1767b287e034959985c1bda784f6d4032
Vadims Malickis <vadims.malickis@rvt.lv>
2023-02-06 13:19:56 +0200

    config and route update

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
-use App\Controllers\HomeController;
 use App\Controllers\AboutController;
 use App\Controllers\ContactController;
+use App\Controllers\HomeController;
 
 return function (Slim\App $app): void {
-    $app->get('/', [HomeController::class, 'index'])->setName('home.index');
-    $app->get('/about', [AboutController::class, 'index'])->setName('about.index');
-    $app->get('/contact', [ContactController::class, 'index'])->setName('contact.index');
+
+    $app->get('/about', [AboutController::class, 'index'])
+        ->setName('about');
+    $app->get('/contact', [ContactController::class, 'index'])
+        ->setName('contact');
+    $app->get('/[{name}]', [HomeController::class, 'index'])
+        ->setArgument('name', 'User')
+        ->setName('home');
 };
