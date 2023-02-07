b9ef358dbd7238568996ca31f1590aa9ada85e9b
Vadims Malickis <vadims.malickis@rvt.lv>
2023-02-07 08:52:08 +0200

    tested connection to database

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,11 +6,18 @@ namespace App\Controllers;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use App;
 
 class HomeController extends Controller
 {
     public function index(Request $request, Response $response, array $args = []): Response
     {
+        $conn = new \mysqli(env('DB_HOST'), env('DB_USER'), env('DB_PASSWORD'), env('DB_NAME'), (int) env('DB_PORT'));
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
         return $this->render($response, 'home/index.twig', [
             'name' => $args['name']
         ]);
