cd2839f392a3a7b41dd158ea8d37cae8f9f9dc71
Vadims Malickis <vadims.malickis@rvt.lv>
2023-02-21 11:59:35 +0200

    playing with js

---
+++ public/assets/js/main.js
@@ -0,0 +1,8 @@
+
+// document.querySelectorAll('.toggle-form')
+//     .forEach(function (element) {
+//     element.addEventListener('click', function (event) {
+//         let clickedBtn = event.target;
+//         clickedBtn.previousElementSibling.style.display = 'block';
+//     });
+// });
