6a688b2c6e101afa6b6583afd50637546627f3c9
Vadims Malickis <117085106+VadimsMalickis@users.noreply.github.com>
2023-02-24 11:19:26 +0200

    Update HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -13,11 +13,6 @@ class HomeController extends Controller
 {
     public function index(Request $request, Response $response, array $args = []): Response
     {
-        $em = $this->em;
-        $user = new User('user' . random_int(0, 999) . '@rvt.lv');
-        $em->persist($user);
-        $em->flush();
-
 
         return $this->render($response, 'home/index.twig', [
             'name' => $args['name']
